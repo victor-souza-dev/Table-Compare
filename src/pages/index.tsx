e507dc3dc5633e2a9494d48cb6a1657cb9fd6a5a
victor-souza-dev <victor.souza2210@gmail.com>
2024-04-05 13:46:59 -0300

    feat: legends

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,65 @@
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 import { TableCompare } from "src/shared/components/TableCompare/TableCompare";
 import {
-  initialDataTableCompare,
-  initialNewDataTableCompare,
+    initialDataTableCompare,
+    initialNewDataTableCompare,
+    statusColorCompare,
 } from "src/shared/constants/TableCompareValues";
 
 export default function Index() {
-  return (
-    <Grid
-      container
-      flexDirection={"column"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      minHeight={"100vh"}
-      p={2}
-    >
-      <Grid
-        mt={10}
-        container
-        maxWidth={"90%"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        gap={10}
-      >
-        <TableCompare
-          currentDataProp={initialDataTableCompare}
-          newDataProp={initialNewDataTableCompare}
-        />
-      </Grid>
-    </Grid>
-  );
+    return (
+        <Grid
+            container
+            flexDirection={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            minHeight={"100vh"}
+            p={2}
+        >
+            <Grid
+                mt={10}
+                container
+                maxWidth={"90%"}
+                alignItems={"center"}
+                justifyContent={"center"}
+                gap={10}
+            >
+                <Grid item container alignItems={'center'} justifyContent={'center'} flexDirection={'row'} columnSpacing={3}>
+                    <Grid item sx={{ display: 'flex' }} flexDirection={'column'} gap={1}>
+                        <Grid item sx={{ display: 'flex', alignItems: 'center', }} gap={1}>
+                            <Box width={15} height={15} bgcolor={statusColorCompare.Created} borderRadius={'100%'} />
+                            <Typography variant={"subtitle2"} fontWeight={'bold'}>
+                                Adicionado
+                            </Typography>
+                        </Grid>
+                        <Grid item sx={{ display: 'flex', alignItems: 'center', }} gap={1}>
+                            <Box width={15} height={15} bgcolor={statusColorCompare.Deleted} borderRadius={'100%'} />
+                            <Typography variant={"subtitle2"} fontWeight={'bold'}>
+                                Removido
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item sx={{ display: 'flex' }} flexDirection={'column'} gap={1}>
+                        <Grid item sx={{ display: 'flex', alignItems: 'center', }} gap={1}>
+                            <Box width={15} height={15} bgcolor={statusColorCompare.Updated} borderRadius={'100%'} />
+                            <Typography variant={"subtitle2"} fontWeight={'bold'}>
+                                Alterado
+                            </Typography>
+                        </Grid>
+                        <Grid item sx={{ display: 'flex', alignItems: 'center', }} gap={1}>
+                            <Box width={15} height={15} bgcolor={statusColorCompare.Reordered} borderRadius={'100%'} />
+                            <Typography variant={"subtitle2"} fontWeight={'bold'}>
+                                Reordenado
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <TableCompare
+                    currentDataProp={initialDataTableCompare}
+                    newDataProp={initialNewDataTableCompare}
+                />
+            </Grid>
+        </Grid>
+    );
 }
--- src/shared/components/TableCompare/TableCompare.tsx
+++ src/shared/components/TableCompare/TableCompare.tsx
@@ -43,6 +43,10 @@ export function TableCompare({ currentDataProp, newDataProp }: IProps) {
         applyStatus();
     }, []);
 
+    function formatTextLimit(text: string, limit: number = 10) {
+        return text.length > limit ? `${text.substring(0, limit)}...` : text;
+    }
+
     function handleMouseEnter(code: string): void {
         if (
             currentData.find((item) => item.code === code)?.status !==
@@ -163,146 +167,146 @@ export function TableCompare({ currentDataProp, newDataProp }: IProps) {
     }
 
     return (
-        <Grid container p={{ xs: 0, md: 4 }}>
+        <Grid container p={{ xs: 0, md: 4 }} gap={4}>
             <Grid container justifyContent={"flex-end"}>
                 <Button
                     variant="contained"
                     color={`${syncScroll ? "success" : "error"}`}
                     onClick={() => setSyncScroll(!syncScroll)}
+                    fullWidth
                 >
-                    Sincronização {syncScroll ? "Ativada" : "Desativada"}
+                    Sincronização das tabelas {syncScroll ? "Ativada" : "Desativada"}
                 </Button>
             </Grid>
-            <Grid container>
-                {["Antes", "Depois"].map((text) => (
-                    <Grid item key={v4()} xs={6} pl={7} pb={2}>
-                        <Typography variant="button">{text}</Typography>
-                    </Grid>
-                ))}
-            </Grid>
-            <Grid container width={"100%"} flexDirection={"row"} gap={2}>
+            <Grid container width={"100%"} flexDirection={"row"} gap={4}>
                 {[currentData, newData].map((data, indexType) => (
-                    <Grid
-                        item
-                        key={`${indexType === 0 ? "scroll-currentData" : "scroll-newData"}`}
-                        ref={indexType === 0 ? currentDataRef : newDataRef}
-                        onScroll={() => onSyncScroll(indexType + 1)}
-                        sx={{
-                            display: "flex",
-                            maxHeight: "600px",
-                            overflow: "auto",
-                        }}
-                        xs={12}
-                        md={5.9}
-                    >
+                    <Grid item xs={12} md={5.85}>
+                        <Grid container flexDirection={'column'}>
+                            <Grid item key={v4()} xs={6} pl={{ md: 7 }} pb={1}>
+                                <Typography variant="button">{indexType === 0 ? "Antes" : "Depois"}</Typography>
+                            </Grid>
+                        </Grid>
                         <Grid
                             item
-                            sx={{ display: "flex" }}
-                            flexDirection={"column"}
-                            pt={7}
-                            gap={0}
+                            key={`${indexType === 0 ? "scroll-currentData" : "scroll-newData"}`}
+                            ref={indexType === 0 ? currentDataRef : newDataRef}
+                            onScroll={() => onSyncScroll(indexType + 1)}
+                            sx={{
+                                display: "flex",
+                                maxHeight: "600px",
+                                overflow: "auto",
+                            }}
+
                         >
-                            {data.map((item, index) => {
-                                let countNone = 0;
+                            <Grid
+                                item
+                                sx={{ display: "flex" }}
+                                flexDirection={"column"}
+                                pt={7}
+                                gap={0}
+                            >
+                                {data.map((item, index) => {
+                                    let countNone = 0;
 
-                                for (let i = 0; i < index; i++) {
-                                    if (data[i].status === StatusEnum.None) {
-                                        countNone++;
-                                    }
-                                }
-
-                                const textNumber =
-                                    item.status === StatusEnum.None ? "#" : index + 1 - countNone;
-
-                                return (
-                                    <Typography
-                                        key={v4()}
-                                        id={`${item.code}:${indexType === 0 ? "currentData" : "newData"}`}
-                                        variant="caption"
-                                        sx={{
-                                            padding: "8px",
-                                            width: 40,
-                                            height: 40,
-                                            margin: "6.5px",
-                                            bgcolor:
-                                                orderHover !== item.code
-                                                    ? ""
-                                                    : item.status === StatusEnum.Updated
-                                                        ? statusColorCompare[StatusEnum.Updated]
-                                                        : statusColorCompare[StatusEnum.Reordered],
-                                            borderRadius: "100%",
-                                            textAlign: "center",
-                                            fontSize: "1rem",
-                                            cursor: "pointer",
-                                        }}
-                                        // onMouseEnter={() => handleMouseEnter(item.code)}
-                                        // onMouseLeave={resetOrderHover}
-                                        onClick={() =>
-                                            scrollToItem(
-                                                `${item.code}:${indexType !== 0 ? "currentData" : "newData"}`,
-                                            )
+                                    for (let i = 0; i < index; i++) {
+                                        if (data[i].status === StatusEnum.None) {
+                                            countNone++;
                                         }
-                                    >
-                                        {textNumber}
-                                    </Typography>
-                                );
-                            })}
-                        </Grid>
-                        <Grid item width={"100%"}>
-                            <TableContainer component={Paper}>
-                                <Table aria-label="simple table">
-                                    <TableHead>
-                                        <TableRow>
-                                            {headTableCompare.map((head) => (
-                                                <TableCell key={v4()} align="left">
-                                                    {head}
-                                                </TableCell>
-                                            ))}
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        {data.map((row) => {
-                                            if (row.status === StatusEnum.None) {
+                                    }
+
+                                    const textNumber =
+                                        item.status === StatusEnum.None ? "#" : index + 1 - countNone;
+
+                                    return (
+                                        <Typography
+                                            key={v4()}
+                                            id={`${item.code}:${indexType === 0 ? "currentData" : "newData"}`}
+                                            variant="caption"
+                                            sx={{
+                                                width: 40,
+                                                height: 40,
+                                                padding: { xs: "8px 8px 8px 0px", md: "8px" },
+                                                margin: { xs: "6.5px 6.5px 6.5px 0px", md: "6.5px" },
+                                                bgcolor:
+                                                    orderHover !== item.code
+                                                        ? ""
+                                                        : item.status === StatusEnum.Updated
+                                                            ? statusColorCompare[StatusEnum.Updated]
+                                                            : statusColorCompare[StatusEnum.Reordered],
+                                                borderRadius: "100%",
+                                                textAlign: "center",
+                                                fontSize: "1rem",
+                                                cursor: "pointer",
+                                            }}
+                                            // onMouseEnter={() => handleMouseEnter(item.code)}
+                                            // onMouseLeave={resetOrderHover}
+                                            onClick={() =>
+                                                scrollToItem(
+                                                    `${item.code}:${indexType !== 0 ? "currentData" : "newData"}`,
+                                                )
+                                            }
+                                        >
+                                            {textNumber}
+                                        </Typography>
+                                    );
+                                })}
+                            </Grid>
+                            <Grid item width={"100%"}>
+                                <TableContainer component={Paper}>
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                {headTableCompare.map((head) => (
+                                                    <TableCell key={v4()} align="left">
+                                                        {head}
+                                                    </TableCell>
+                                                ))}
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {data.map((row) => {
+                                                if (row.status === StatusEnum.None) {
+                                                    return (
+                                                        <TableRow
+                                                            key={v4()}
+                                                            sx={{
+                                                                backgroundImage:
+                                                                    "linear-gradient(45deg, transparent 25%, #00000025 25%, #00000024 50%, transparent 50%, transparent 75%, #00000025 75%, #00000025)",
+                                                                backgroundSize: "8px 8px",
+                                                                height: 53.02,
+                                                            }}
+                                                        >
+                                                            <TableCell align="left" />
+                                                            <TableCell align="left" />
+                                                            <TableCell align="left" />
+                                                        </TableRow>
+                                                    );
+                                                }
+
                                                 return (
                                                     <TableRow
                                                         key={v4()}
                                                         sx={{
-                                                            backgroundImage:
-                                                                "linear-gradient(45deg, transparent 25%, #00000025 25%, #00000024 50%, transparent 50%, transparent 75%, #00000025 75%, #00000025)",
-                                                            backgroundSize: "8px 8px",
-                                                            height: 53.02,
+                                                            "&:last-child td, &:last-child th": { border: 0 },
+                                                            bgcolor:
+                                                                row.code === orderHover &&
+                                                                    row.status !== StatusEnum.Updated
+                                                                    ? statusColorCompare[StatusEnum.Reordered]
+                                                                    : statusColorCompare[row.status],
                                                         }}
+                                                    // onMouseEnter={() => handleMouseEnter(row.code)}
+                                                    // onMouseLeave={resetOrderHover}
                                                     >
-                                                        <TableCell align="left" />
-                                                        <TableCell align="left" />
-                                                        <TableCell align="left" />
+                                                        <TableCell align="left">{formatTextLimit(row.code)}</TableCell>
+                                                        <TableCell align="left">{formatTextLimit(row.name)}</TableCell>
+                                                        <TableCell align="left">{formatTextLimit(row.type)}</TableCell>
                                                     </TableRow>
                                                 );
-                                            }
-
-                                            return (
-                                                <TableRow
-                                                    key={v4()}
-                                                    sx={{
-                                                        "&:last-child td, &:last-child th": { border: 0 },
-                                                        bgcolor:
-                                                            row.code === orderHover &&
-                                                                row.status !== StatusEnum.Updated
-                                                                ? statusColorCompare[StatusEnum.Reordered]
-                                                                : statusColorCompare[row.status],
-                                                    }}
-                                                // onMouseEnter={() => handleMouseEnter(row.code)}
-                                                // onMouseLeave={resetOrderHover}
-                                                >
-                                                    <TableCell align="left">{row.code}</TableCell>
-                                                    <TableCell align="left">{row.name}</TableCell>
-                                                    <TableCell align="left">{row.type}</TableCell>
-                                                </TableRow>
-                                            );
-                                        })}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
+                                            })}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
                         </Grid>
                     </Grid>
                 ))}
